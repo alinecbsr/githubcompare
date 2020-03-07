6044e0708a976a6cd64e99b865c9d492b158a0f2
Carvalho, Aline De <aline.de.carvalho@accenture.com>
2020-03-07 12:04:11 -0300

    Global configuration, eslint airbnb and editorconfig implemented

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: '#F00',
+  font-size: 32px,
+`
+
+const App = () => <Main />
+
+export default App
---
+++ src/pages/Main.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Main = () => (
+  <div>
+    <img src="" alt="GitHub Compare" />
+  </div>
+)
+
+export default Main

ef3881f43e05e3878c8d0fd2e9795b0bd63391cb
Carvalho, Aline De <aline.de.carvalho@accenture.com>
2020-03-08 21:23:52 -0300

    Searching API repositories

--- src/components/compareList/index.js
+++ src/components/compareList/index.js
@@ -1,17 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { Container, Repository } from "./styles";
 
 const CompareList = ({ repositories }) => (
   <Container>
     {repositories.map(repository => (
-      <Repository>
+      <Repository key={repository.id}>
         <header>
           <img src={repository.owner.avatar_url} alt={repository.owner.login} />
           <strong>{repository.name}</strong>
           <small>{repository.owner.login}</small>
         </header>
-
         <ul>
           <li>
             {repository.stargazers_count} <small>stars</small>
@@ -30,5 +30,22 @@ const CompareList = ({ repositories }) => (
     ))}
   </Container>
 );
+
+CompareList.propTypes = {
+  repositories: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      owner: PropTypes.shape({
+        login: PropTypes.string,
+        avatar_url: PropTypes.string
+      }),
+      stargazers_count: PropTypes.number,
+      forks_count: PropTypes.number,
+      open_issues_count: PropTypes.number,
+      pushed_at: PropTypes.shape
+    })
+  ).isRequired
+};
 
 export default CompareList;
